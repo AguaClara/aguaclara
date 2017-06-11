8ecf6372ac4e93f670456777644392cfafc65b8a
Monroe Weber-Shirk <monroews@gmail.com>
2017-06-11 10:04:56 -0400

    added spaces

--- utility.py
+++ utility.py
@@ -1,6 +1,8 @@
 # units allows us to include units in all of our calculations
+
 from AguaClara_design.units import unit_registry as u
-#It would be nice to display answers with a reasonable number of significant digits
+
+
 # x is a number that may include units. n is the number of significant digits to display.
 
 # We need to fix the formatting so that it doesn't display trailing zeroes that are not significant.
