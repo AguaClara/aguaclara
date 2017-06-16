24694e48fe0eb8266c8d02bad5240f01b48c7c70
karannewatia <kn348@cornell.edu>
2017-06-16 13:06:47 -0400

    67

--- physchem.py
+++ physchem.py
@@ -157,15 +157,11 @@ def flow_orifice_vert(D,h,ratio_VC_orifice):
         D.to(u.m)
         h=h.magnitude
         D=D.magnitude
-        print(h)
-        print(D)
-        
-      
+
         Q=scipy.integrate.quad(lambda z: D*math.sin(math.acos(z/(D/2)))*((h-z)**(1/2)),-D/2,min(D/2,h))
         Qnew=Q[0]
         Q=ratio_VC_orifice*((2*9.81)**(1/2))*Qnew
-#        Q=ratio_VC_orifice*((2*9.81)**(1/2))*scipy.integrate.quad(lambda z: D*math.sin(math.acos(z/(D/2)))*((h-z)**(1/2)),-D/2,min(D/2,h))
-        print (Q)
+       
         return Q*(u.L/u.s)
     else:
        return 0*(u.L/u.s)
