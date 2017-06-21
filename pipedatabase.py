5366296684946e103ae8a521b58965f1bf20e869
SageWS <jmweber14@earlham.edu>
2017-06-21 17:12:24 -0400

    added spacing for increased readability

--- pipedatabase.py
+++ pipedatabase.py
@@ -26,41 +26,41 @@ def OD(ND):
     # Extract the magnitude in inches from the nominal diameter
     # Find the index of the closest nominal diameter (Should this be changed to find the next largest ND?)
     # Take the values of the array, subtract the ND, take the absolute value, find the index of the minimium value
-    myindex=(np.abs(np.array(pipedb['NDinch'])-(ND.to(u.inch)).magnitude)).argmin()
-    return pipedb.iloc[myindex,1]*u.inch
+    myindex = (np.abs(np.array(pipedb['NDinch']) - (ND.to(u.inch)).magnitude)).argmin()
+    return pipedb.iloc[myindex, 1] * u.inch
 
 # Returns the inner diameter for SDR (standard diameter ratio) pipes. 
 # For these pipes the wall thickness is the outer diameter divided by the SDR.
-def ID_SDR(ND,SDR):
-    ID=OD(ND)*(SDR-2)/SDR
+def ID_SDR(ND, SDR):
+    ID = OD(ND) * (SDR-2) / SDR
     return ID
 
 # Returns the inner diameter for schedule 40 pipes. The wall thickness for these pipes is in the pipedb.
 def ID_sch40(ND):
     #take the values of the array, subtract the ND, take the absolute value, find the index of the minimium value
-    myindex=(np.abs(np.array(pipedb['NDinch'])-(ND.to(u.inch)).magnitude)).argmin()
-    return (pipedb.iloc[myindex,1]-2*(pipedb.iloc[myindex,5]))*u.inch
+    myindex = (np.abs(np.array(pipedb['NDinch']) - (ND.to(u.inch)).magnitude)).argmin()
+    return (pipedb.iloc[myindex, 1] - 2*(pipedb.iloc[myindex,5])) * u.inch
 
 # Returns an array of available nominal diameters that are commonly used based on the 'Used' column in the pipedb
 def ND_all_available():
-    ND_all_available=[]
+    ND_all_available = []
     for i in range(len(pipedb['NDinch'])):
-        if pipedb.iloc[i,4]==1:
+        if pipedb.iloc[i,4] == 1:
             ND_all_available.append((pipedb['NDinch'][i]))
     return ND_all_available*u.inch
 
 # Returns an array of inner diameters that are commonly used based on the 'Used' column in the pipedb and given an SDR
 def ID_SDR_all_available(SDR):
-    ID=[]
-    ND=ND_all_available()
+    ID = []
+    ND = ND_all_available()
     for i in range(len(ND)):
-        ID.append(ID_SDR(ND[i],SDR).magnitude)
+        ID.append(ID_SDR(ND[i], SDR).magnitude)
     return ID*u.inch
 
 # Return an available ND given an ID and a schedule
 # Take the values of the array, compare to the ID, find the index of the first value greater or equal
 def ND_SDR_available(ID,SDR):
-    myindex=np.argmax((np.array(ID_SDR_all_available(SDR))>=(ID.to(u.inch)).magnitude))
+    myindex = np.argmax((np.array(ID_SDR_all_available(SDR)) >= (ID.to(u.inch)).magnitude))
     return ND_all_available()[myindex]
 
 # Returns the minimum ND that is available.
@@ -68,5 +68,5 @@ def ND_available(NDguess):
     # Extract the magnitude in inches from the nominal diameter
     # Find the index of the closest nominal diameter
     # Take the values of the array, subtract the ND, take the absolute value, find the index of the minimium value
-    myindex=(ND_all_available() >= NDguess)
+    myindex = (ND_all_available() >= NDguess)
     return min(ND_all_available()[myindex])
