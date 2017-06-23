6ccc3b00a3e2d78f7186cd71db5f7770447c7779
karannewatia <kn348@cornell.edu>
2017-06-23 15:11:39 -0400

    s

--- utility.py
+++ utility.py
@@ -11,15 +11,14 @@ import math
 
 from units import unit_registry as u
 
+#We need to fix the formatting so that it doesn't display trailing zeroes
+#that are not significant.
 
-"""x is a number that may include units. n is the number of significant
-digits to display.
-We need to fix the formatting so that it doesn't display trailing zeroes
-that are not significant.
-"""
 def sig(x,n):
-    # Check to see if the quantity x includes units so we can strip the
-    # units and then reattach them at the end.
+    """x is a number that may include units. n is the number of significant
+digits to display."""
+# Check to see if the quantity x includes units so we can strip the
+# units and then reattach them at the end.
     if type(x) == type(1*u.m):
         xmag=float(x.magnitude)
         xunit=x.units
@@ -36,12 +35,13 @@ def sig(x,n):
            xmag=round(xmag, n-1-int(math.floor(math.log10(abs(xmag)))))
            return xmag
 
-"""This function returns the smallest multiple of 'step' greater than or
+
+def stepceil_with_units(param, step, unit):
+    """This function returns the smallest multiple of 'step' greater than or
 equal to 'param' and outputs the result in Pint units. 
 This function is unit-aware and functions without requiring translation
 so long as 'param' and 'unit' are of the same diemnsionality.
 """
-def stepceil_with_units(param, step, unit):
     counter = 0 * unit
     while counter < param.to(unit):
         counter += step * u.inch
