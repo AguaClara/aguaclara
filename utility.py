d94a35b292134fcb057f69b28faa6e4d806b40b5
SageWS <jmweber14@earlham.edu>
2017-06-23 14:39:21 -0400

    Updated files for user-friendliness
    Added spacing and docstrings. Variable names still need to be changed to reflect our standards.

--- utility.py
+++ utility.py
@@ -3,7 +3,7 @@ Created on Sun Jun 11
 
 @author: Monroe Weber-Shirk
 
-Last modified: Thu Jun 22 2017 
+Last modified: Fri Jun 23 2017 
 By: Sage Weber-Shirk
 """
 # units allows us to include units in all of our calculations
@@ -12,11 +12,14 @@ import math
 from AguaClara_design.units import unit_registry as u
 
 
-# x is a number that may include units. n is the number of significant digits to display.
-
-# We need to fix the formatting so that it doesn't display trailing zeroes that are not significant.
+"""x is a number that may include units. n is the number of significant
+digits to display.
+We need to fix the formatting so that it doesn't display trailing zeroes
+that are not significant.
+"""
 def sig(x,n):
-    #Check to see if the quantity x includes units so we can strip the units and then reattach them at the end.
+    # Check to see if the quantity x includes units so we can strip the
+    # units and then reattach them at the end.
     if type(x) == type(1*u.m):
         xmag=float(x.magnitude)
         xunit=x.units
@@ -27,7 +30,11 @@ def sig(x,n):
         xmag=round(xmag, n-1-int(math.floor(math.log10(abs(xmag)))))
     return '{:~P}'.format(u.Quantity(xmag,xunit))
 
-
+"""This function returns the smallest multiple of 'step' greater than or
+equal to 'param' and outputs the result in Pint units. 
+This function is unit-aware and functions without requiring translation
+so long as 'param' and 'unit' are of the same diemnsionality.
+"""
 def stepceil_with_units(param, step, unit):
     counter = 0 * unit
     while counter < param.to(unit):
