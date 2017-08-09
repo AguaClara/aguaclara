ce67c44b0ad2dbda5db40d411651c5f0f958e517
SageWS <jmweber14@earlham.edu>
2017-08-09 16:43:20 -0400

    added graphs
    only the 50NTU set is graphed right now, and the data isn't right for the fit lines - they need to be fixed, one of the inputs must be off.

--- graphing_work.py
+++ graphing_work.py
@@ -10,12 +10,14 @@ from matplotlib import pyplot as plt
 try:
     from AguaClara_design.units import unit_registry as u
     from AguaClara_design import physchem as pc
+    from AguaClara_design import floc_model as floc
 except ModuleNotFoundError:
     from units import unit_registry as u
     import physchem as pc
+    import floc_model as floc
     
-    
-coag = np.array([0.53, 1.06, 1.59, 2.11, 2.56] * u.mg/u.L)
+k = 0.23
+coag = np.array([0.53, 1.06, 1.59, 2.11, 2.56]) * u.mg/u.L
 conc_humic_acid = np.array([0, 3, 6, 9, 12, 15] * u.mg/u.L)
 #dataset[0] is the 50NTU, dataset[1] is the 100NTU.
 #Within both subgroups, [0] is the pC.0, ranging evenly up to [5] which is the 
@@ -35,4 +37,39 @@ dataset = np.array([[[0.634, 0.729, 0.891, 1.062, 1.205],
                      [0.084, 0.157, 0.566, 1.084, 1.314]
                      ]
                     ])
-x
+
+plt.plot(coag, dataset[0][0], 'r.', coag, dataset[0][1], 'b.', coag, dataset[0][2], 'g.',
+         coag, dataset[0][3], 'm.', coag, dataset[0][4], 'c.', coag, dataset[0][5], 'y.')
+
+coagGraph = np.arange(1 * 10**-4, 25.1 * 10**-4, 1 * 10**-4) * u.kg/u.m**3
+#I wish there was a cleaner way to assign these but I can't think
+# of what it would be.
+line0mg = floc.pc_viscous(4.833 * u.mW/u.kg, 25 * u.degC, 302 * u.s, 
+                          floc.DIAM_CLAY, 3/8 * u.inch, floc.DIAM_CLAY, 
+                          floc.DENS_CLAY, 50 * u.NTU, coagGraph, 0 * u.mg/u.L, 
+                          floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line3mg = floc.pc_viscous(4.833 * u.mW/u.kg, 25 * u.degC, 302 * u.s, 
+                          floc.DIAM_CLAY, 3/8 * u.inch, floc.DIAM_CLAY, 
+                          floc.DENS_CLAY, 50 * u.NTU, coagGraph, 3 * u.mg/u.L, 
+                          floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line6mg = floc.pc_viscous(4.833 * u.mW/u.kg, 25 * u.degC, 302 * u.s, 
+                          floc.DIAM_CLAY, 3/8 * u.inch, floc.DIAM_CLAY, 
+                          floc.DENS_CLAY, 50 * u.NTU, coagGraph, 6 * u.mg/u.L, 
+                          floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line9mg = floc.pc_viscous(4.833 * u.mW/u.kg, 25 * u.degC, 302 * u.s, 
+                          floc.DIAM_CLAY, 3/8 * u.inch, floc.DIAM_CLAY, 
+                          floc.DENS_CLAY, 50 * u.NTU, coagGraph, 9 * u.mg/u.L, 
+                          floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line12mg = floc.pc_viscous(4.833 * u.mW/u.kg, 25 * u.degC, 302 * u.s, 
+                          floc.DIAM_CLAY, 3/8 * u.inch, floc.DIAM_CLAY, 
+                          floc.DENS_CLAY, 50 * u.NTU, coagGraph, 12 * u.mg/u.L, 
+                          floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line15mg = floc.pc_viscous(4.833 * u.mW/u.kg, 25 * u.degC, 302 * u.s, 
+                          floc.DIAM_CLAY, 3/8 * u.inch, floc.DIAM_CLAY, 
+                          floc.DENS_CLAY, 50 * u.NTU, coagGraph, 15 * u.mg/u.L, 
+                          floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+
+x = coagGraph.to(u.mg/u.L).magnitude
+plt.plot(x, line0mg, 'r', x, line3mg, 'b', x, line6mg, 'g', 
+         x, line9mg, 'm', x, line12mg, 'c', x, line15mg, 'y')
+plt.show()
