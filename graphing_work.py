5cfec10c54523ef8ab86e1c324cb7957d12a217e
SageWS <jmweber14@earlham.edu>
2017-08-10 11:20:07 -0400

    Graphs now complete, but pc_viscous still seems inaccurate

--- graphing_work.py
+++ graphing_work.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug  8 16:00:38 2017
+Created on Tue Aug 8 16:00:38 2017
 
 @author: Sage Weber-Shirk
+
+Last modified: Thu Aug 10 2016
+By: Sage Weber-Shirk
 """
 
 import numpy as np
@@ -16,7 +19,7 @@ except ModuleNotFoundError:
     import physchem as pc
     import floc_model as floc
     
-k = 0.23
+k = 0.18
 coag = np.array([0.53, 1.06, 1.59, 2.11, 2.56]) * u.mg/u.L
 conc_humic_acid = np.array([0, 3, 6, 9, 12, 15] * u.mg/u.L)
 #dataset[0] is the 50NTU, dataset[1] is the 100NTU.
@@ -39,6 +42,10 @@ dataset = np.array([[[0.634, 0.729, 0.891, 1.062, 1.205],
                     ])
 
 coagGraph = np.arange(1 * 10**-4, 25.1 * 10**-4, 1 * 10**-4) * u.kg/u.m**3
+enerDis = 4.833 * u.mW/u.kg
+temperature = 25 * u.degC
+resTime = 302 * u.s
+tubeDiam = 3/8 * u.inch
 #Begin graphing the 50NTU datasets
 plt.subplot(121)
 plt.title('50 NTU Graph')
@@ -51,34 +58,34 @@ plt.plot(coag, dataset[0][0], 'r.', coag, dataset[0][1], 'b.', coag, dataset[0][
 
 #I wish there was a cleaner way to assign these but I can't think
 # of what it would be.
-line0mg = floc.pc_viscous(4.833 * u.mW/u.kg, 25 * u.degC, 302 * u.s, 
-                          floc.DIAM_CLAY, 3/8 * u.inch, floc.DIAM_CLAY, 
-                          floc.DENS_CLAY, 50 * u.NTU, coagGraph, 0 * u.mg/u.L, 
-                          floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
-line3mg = floc.pc_viscous(4.833 * u.mW/u.kg, 25 * u.degC, 302 * u.s, 
-                          floc.DIAM_CLAY, 3/8 * u.inch, floc.DIAM_CLAY, 
-                          floc.DENS_CLAY, 50 * u.NTU, coagGraph, 3 * u.mg/u.L, 
-                          floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
-line6mg = floc.pc_viscous(4.833 * u.mW/u.kg, 25 * u.degC, 302 * u.s, 
-                          floc.DIAM_CLAY, 3/8 * u.inch, floc.DIAM_CLAY, 
-                          floc.DENS_CLAY, 50 * u.NTU, coagGraph, 6 * u.mg/u.L, 
-                          floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
-line9mg = floc.pc_viscous(4.833 * u.mW/u.kg, 25 * u.degC, 302 * u.s, 
-                          floc.DIAM_CLAY, 3/8 * u.inch, floc.DIAM_CLAY, 
-                          floc.DENS_CLAY, 50 * u.NTU, coagGraph, 9 * u.mg/u.L, 
-                          floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
-line12mg = floc.pc_viscous(4.833 * u.mW/u.kg, 25 * u.degC, 302 * u.s, 
-                          floc.DIAM_CLAY, 3/8 * u.inch, floc.DIAM_CLAY, 
-                          floc.DENS_CLAY, 50 * u.NTU, coagGraph, 12 * u.mg/u.L, 
-                          floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
-line15mg = floc.pc_viscous(4.833 * u.mW/u.kg, 25 * u.degC, 302 * u.s, 
-                          floc.DIAM_CLAY, 3/8 * u.inch, floc.DIAM_CLAY, 
-                          floc.DENS_CLAY, 50 * u.NTU, coagGraph, 15 * u.mg/u.L, 
-                          floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line0mg50 = floc.pc_viscous(enerDis, temperature, resTime,
+                            tubeDiam, floc.DIAM_CLAY, 
+                            floc.DENS_CLAY, 50 * u.NTU, coagGraph, 0 * u.mg/u.L, 
+                            floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line3mg50 = floc.pc_viscous(enerDis, temperature, resTime,
+                            tubeDiam, floc.DIAM_CLAY, 
+                            floc.DENS_CLAY, 50 * u.NTU, coagGraph, 3 * u.mg/u.L, 
+                            floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line6mg50 = floc.pc_viscous(enerDis, temperature, resTime,
+                            tubeDiam, floc.DIAM_CLAY, 
+                            floc.DENS_CLAY, 50 * u.NTU, coagGraph, 6 * u.mg/u.L, 
+                            floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line9mg50 = floc.pc_viscous(enerDis, temperature, resTime,
+                            tubeDiam, floc.DIAM_CLAY, 
+                            floc.DENS_CLAY, 50 * u.NTU, coagGraph, 9 * u.mg/u.L, 
+                            floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line12mg50 = floc.pc_viscous(enerDis, temperature, resTime,
+                            tubeDiam, floc.DIAM_CLAY, 
+                            floc.DENS_CLAY, 50 * u.NTU, coagGraph, 12 * u.mg/u.L, 
+                            floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line15mg50 = floc.pc_viscous(enerDis, temperature, resTime,
+                            tubeDiam, floc.DIAM_CLAY, 
+                            floc.DENS_CLAY, 50 * u.NTU, coagGraph, 15 * u.mg/u.L, 
+                            floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
 
 x = coagGraph.to(u.mg/u.L)
-plt.plot(x, line0mg, 'r', x, line3mg, 'b', x, line6mg, 'g', 
-         x, line9mg, 'm', x, line12mg, 'c', x, line15mg, 'y')
+plt.plot(x, line0mg50, 'r', x, line3mg50, 'b', x, line6mg50, 'g', 
+         x, line9mg50, 'm', x, line12mg50, 'c', x, line15mg50, 'y')
 
 
 #Begin graphing the 100NTU datasets
@@ -87,27 +94,39 @@ plt.title('100 NTU Graph')
 plt.ylabel('pC*')
 plt.xlabel('coagulant dosage (mg/L)')
 
+plt.plot(coag, dataset[1][0], 'r.', coag, dataset[1][1], 'b.', coag, dataset[1][2], 'g.',
+         coag, dataset[1][3], 'm.', coag, dataset[1][4], 'c.', coag, dataset[1][5], 'y.')
+
+line0mg100 = floc.pc_viscous(enerDis, temperature, resTime,
+                            tubeDiam, floc.DIAM_CLAY, 
+                            floc.DENS_CLAY, 100 * u.NTU, coagGraph, 0 * u.mg/u.L, 
+                            floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line3mg100 = floc.pc_viscous(enerDis, temperature, resTime,
+                            tubeDiam, floc.DIAM_CLAY, 
+                            floc.DENS_CLAY, 100 * u.NTU, coagGraph, 3 * u.mg/u.L, 
+                            floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line6mg100 = floc.pc_viscous(enerDis, temperature, resTime,
+                            tubeDiam, floc.DIAM_CLAY, 
+                            floc.DENS_CLAY, 100 * u.NTU, coagGraph, 6 * u.mg/u.L, 
+                            floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line9mg100 = floc.pc_viscous(enerDis, temperature, resTime,
+                            tubeDiam, floc.DIAM_CLAY, 
+                            floc.DENS_CLAY, 100 * u.NTU, coagGraph, 9 * u.mg/u.L, 
+                            floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line12mg100 = floc.pc_viscous(enerDis, temperature, resTime,
+                            tubeDiam, floc.DIAM_CLAY, 
+                            floc.DENS_CLAY, 100 * u.NTU, coagGraph, 12 * u.mg/u.L, 
+                            floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+line15mg100 = floc.pc_viscous(enerDis, temperature, resTime,
+                            tubeDiam, floc.DIAM_CLAY, 
+                            floc.DENS_CLAY, 100 * u.NTU, coagGraph, 15 * u.mg/u.L, 
+                            floc.HumicAcid, floc.PACl, k, floc.RATIO_HEIGHT_DIAM)
+
+x = coagGraph.to(u.mg/u.L)
+plt.plot(x, line0mg100, 'r', x, line3mg100, 'b', x, line6mg100, 'g', 
+         x, line9mg100, 'm', x, line12mg100, 'c', x, line15mg100, 'y')
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#And now we display our graph!
 plt.show()
