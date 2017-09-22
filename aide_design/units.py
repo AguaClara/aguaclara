5cd456fe63a2ea28dde20a464d68f44a81deebf6
ethan92429 <ethan.keller@gmail.com>
2017-09-22 13:13:54 -0600

    beginning work on a pipeline function

--- aide_design/units.py
+++ aide_design/units.py
@@ -22,21 +22,40 @@ modules.
 
 import os
 import pint
+import pandas as pd
 
 unit_registry = pint.UnitRegistry(system='mks', autoconvert_offset_to_baseunit=True)
 
 # default formatting includes 4 significant digits. This can be overridden on a per-print basis
 #  with print('{:.3f}'.format(3*ureg.m /9))
 unit_registry.default_format = '.4g'
+pd.options.display.float_format = '{:,.4g}'.format
 
 
 def set_sig_fig(n: int):
-    """Set the default number of significant figures used to print pint 
+    """Set the default number of significant figures used to print pint, pandas and numpy values
     quantities. Defaults to 4.
     
     Args:
         n: number of significant figures to display
+
+    Example:
+        import aide_design
+        from aide_design.units import unit_registry as u
+        h=2.5532532522352543*u.m
+        e = 25532532522352543*u.m
+        print('h before sigfig adjustment: ',h)
+        print('e before sigfig adjustment: ',e)
+        aide_design.units.set_sig_fig(10)
+        print('h after sigfig adjustment: ',h)
+        print('e after sigfig adjustment: ',e)
+
+        h before sigfig adjustment:  2.553 meter
+        e before sigfig adjustment:  2.553e+16 meter
+        h after sigfig adjustment:  2.553253252 meter
+        e after sigfig adjustment:  2.553253252e+16 meter
     """
     unit_registry.default_format = '.' + str(n) + 'g'
+    pd.options.display.float_format = ('{:,.' + str(n) + '}').format
 
 unit_registry.load_definitions(os.path.join(os.path.dirname(__file__), "data/unit_definitions.txt"))
