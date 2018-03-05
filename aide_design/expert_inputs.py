e9e037002c1f874478c9ea679c81ecc5d6894cc5
fletchapin <fletchapin@gmail.com>
2018-03-05 14:08:22 -0500

    Fixed formatting of deprecation warning

--- aide_design/expert_inputs.py
+++ aide_design/expert_inputs.py
@@ -6,10 +6,12 @@ from aide_design.units import unit_registry as u
 
 import warnings
 warnings.simplefilter('default')
-warnings.warn("The module expert_inputs.py is deprecated. Please use constants"
+warnings.warn(
+              "The module expert_inputs.py is deprecated. Please use constants"
               + ".py which is imported as con instead. Some global variables" +
               "have been moved to optional_inputs.py which is imported as opt",
-              DeprecationWarning)
+              DeprecationWarning
+              )
 
 #####tabulated constants
 
