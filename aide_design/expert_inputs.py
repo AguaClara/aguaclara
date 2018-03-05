1b8b9fc84da2a3f2e59df9e27630b38e72a83f23
fletchapin <fletchapin@gmail.com>
2018-03-05 14:06:49 -0500

    Trying to get warning to be called without flag

--- aide_design/expert_inputs.py
+++ aide_design/expert_inputs.py
@@ -5,6 +5,7 @@ scientific principles which will be used in AguaClara plant design.
 from aide_design.units import unit_registry as u
 
 import warnings
+warnings.simplefilter('default')
 warnings.warn("The module expert_inputs.py is deprecated. Please use constants"
               + ".py which is imported as con instead. Some global variables" +
               "have been moved to optional_inputs.py which is imported as opt",
