7cade18fabe63a815bb13d8935fb503893cc8df2
zoemaisel <31140352+zam7@users.noreply.github.com>
2018-03-10 16:27:09 -0500

    added some more optional inputs for sed tank

--- aide_design/optional_inputs.py
+++ aide_design/optional_inputs.py
@@ -26,4 +26,18 @@ THICKNESS_PLANT_FLOOR = 0.2 * u.m  # plant floor slab thickness
 
 THICKNESS_SED_WALL = 0.15 * u.m  # thickness of sed tank dividing wall
 
-FLOC_BLANKET_HEIGHT = 0.25 * u.m # vertical height of floc blanket from peak of slope to weir
+FLOC_BLANKET_HEIGHT = 0.25 * u.m  # vertical height of floc blanket from peak of slope to weir
+
+ND_jet_reverser = 3 * u.inch  # nominal diameter of pipe used for jet reverser in bottom of set tank
+
+ND_diffuser_pipe = 4 * u.cm  # nominal diameter of pipe used to make diffusers
+
+W_diffuser_opening = 0.3175 * u.cm  # opening width of diffusers
+
+L_diffuser_opening  # NEED VALUE
+
+L_diffuser = 15 * u.cm  # vertical length of diffuser
+
+SDR_diffuser = 26  # SDR of diffuser pipe
+
+B_diffusers = 5 * u.cm  # center to center spacing beteen diffusers
