ce8580254b035de723bce782f1696943febe0169
zoemaisel <31140352+zam7@users.noreply.github.com>
2018-03-10 16:51:57 -0500

    working on sed.py

--- aide_design/optional_inputs.py
+++ aide_design/optional_inputs.py
@@ -7,19 +7,19 @@ from aide_design.units import unit_registry as u
 ## ETLF
 
 # Entrance Tank
-L_ET_MAX = 2.2*u.m
+L_ET_MAX = 2.2 * u.m
 
 # LFOM
-HL_LFOM = 20*u.cm
+HL_LFOM = 20 * u.cm
 
 S_LFOM_ORIFICE = 1 * u.cm  # minimum wall distance between orifices, for lfom structural stability
 
 # Flocculator
-HL_FLOC = 0.4*u.m
+HL_FLOC = 0.4 * u.m
 
 COLL_POT = 37000  # collision potential, also referred to as Gt
 
-FREEBOARD = 10*u.cm
+FREEBOARD = 10 * u.cm
 
 # Sedimentation tank
 THICKNESS_PLANT_FLOOR = 0.2 * u.m  # plant floor slab thickness
@@ -28,16 +28,18 @@ THICKNESS_SED_WALL = 0.15 * u.m  # thickness of sed tank dividing wall
 
 FLOC_BLANKET_HEIGHT = 0.25 * u.m  # vertical height of floc blanket from peak of slope to weir
 
-ND_jet_reverser = 3 * u.inch  # nominal diameter of pipe used for jet reverser in bottom of set tank
+ND_JET_REVERSER = 3 * u.inch  # nominal diameter of pipe used for jet reverser in bottom of set tank
 
-ND_diffuser_pipe = 4 * u.cm  # nominal diameter of pipe used to make diffusers
+ND_DIFFUSER_PIPE = 4 * u.cm  # nominal diameter of pipe used to make diffusers
 
-W_diffuser_opening = 0.3175 * u.cm  # opening width of diffusers
+W_DIFFUSER_OPENING = 0.3175 * u.cm  # opening width of diffusers
 
-L_diffuser_opening  # NEED VALUE
+L_DIFFUSER_OPENING = 1 * u.cm  # NEED VALUE BECAUSE THIS IS A GUESS
 
-L_diffuser = 15 * u.cm  # vertical length of diffuser
+L_DIFFUSER = 15 * u.cm  # vertical length of diffuser
 
-SDR_diffuser = 26  # SDR of diffuser pipe
+SDR_DIFFUSER = 26  # SDR of diffuser pipe
 
-B_diffusers = 5 * u.cm  # center to center spacing beteen diffusers
+B_DIFFUSER = 5 * u.cm  # center to center spacing beteen diffusers
+
+HL_OUTLET_MAN = 5 * u.cm  # head loss through the outlet manifold
--- aide_design/unit_process_design/sed.py
+++ aide_design/unit_process_design/sed.py
@@ -25,5 +25,31 @@ def n_sed_plates_max(dist_center_sed_plate):
     >>>
 
     """
-    return math.floor((mat.LENGTH_SED_PLATE_CANTILEVERED/dist_center_sed_plate
-                      * np.tan(con.ANGLE_SED_PLATE.to(u.rad).magnitude)) + 1)
+    return math.floor((mat.LENGTH_SED_PLATE_CANTILEVERED/dist_center_sed_plate * np.tan(con.ANGLE_SED_PLATE.to(u.rad).magnitude)) + 1)
+
+print(n_sed_plates_max(2.5*u.cm))
+
+@u.wraps(u.L/u.s, [u.m, u.m], False)
+def diffuser_max_flow(W_sed_tank, L_sed_diffuser_outer):
+    """Return the flow through each diffuser.
+
+    Parameters
+    ----------
+    var1 : float
+        Width of the sedimentation tank
+
+    var2 : float
+        Outer length of the diffusers
+
+    Returns
+    -------
+    float
+        Flow through each diffuser in the sedimentation tank
+
+    Examples
+    --------
+    >>> from aide_design.play import*
+    >>>
+
+    """
+    return con.VEL_SED_UP_BOD * W_sed * L_sed_diffuser_outer
