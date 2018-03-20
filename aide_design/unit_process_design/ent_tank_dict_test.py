404fc3bc24b15de742e87f3940e5ef730fe652cd
zoemaisel <31140352+zam7@users.noreply.github.com>
2018-03-20 16:32:26 -0400

    made sure that all ent_tank dictionaries were put in as ent_tank_inputs

--- aide_design/unit_process_design/ent_tank_dict_test.py
+++ aide_design/unit_process_design/ent_tank_dict_test.py
@@ -12,14 +12,14 @@ ent_tank_dict = {'sdr': 26, 'K_MINOR_PIPE_ENTRANCE': 0.5,
 
 @u.wraps(u.inch, [u.m**3/u.s, u.degK, u.m, None], False)
 def drain_OD(q_plant, temp, depth_end, ent_tank_inputs=ent_tank_dict):
-    """Return the nominal diameter of the entrance tank drain pipe. Depth at the
+    """Return the outer diameter of the entrance tank drain pipe. Depth at the
     end of the flocculator is used for headloss and length calculation inputs in
     the diam_pipe calculation."""
     nu = pc.viscosity_kinematic(temp)
-    K_minor = (ent_tank_dict['K_MINOR_PIPE_ENTRANCE'] +
-               ent_tank_dict['K_MINOR_PIPE_EXIT'] + ent_tank_dict['K_MINOR_EL90'])
-    drain_ID = pc.diam_pipe(q_plant, depth_end, depth_end, nu, ent_tank_dict['PIPE_ROUGH_PVC'], K_minor)
-    drain_ND = pipe.ND_SDR_available(drain_ID, ent_tank_dict['sdr'])
+    K_minor = (ent_tank_inputs['K_MINOR_PIPE_ENTRANCE'] +
+               ent_tank_inputs['K_MINOR_PIPE_EXIT'] + ent_tank_inputs['K_MINOR_EL90'])
+    drain_ID = pc.diam_pipe(q_plant, depth_end, depth_end, nu, ent_tank_inputs['PIPE_ROUGH_PVC'], K_minor)
+    drain_ND = pipe.ND_SDR_available(drain_ID, ent_tank_inputs['sdr'])
     return pipe.OD(drain_ND)
 
 @u.wraps(None, [u.m**3/u.s, u.m, None], False)
@@ -27,24 +27,25 @@ def num_plates_ent_tank(q_plant, W_chan, ent_tank_inputs=ent_tank_dict):
     """Return the number of plates in the entrance tank.
 
     This number minimizes the total length of the plate settler unit."""
-    N_plates = np.ceil(np.sqrt(q_plant/(ent_tank_dict['B_plate'].to(u.m).magnitude
-                       * W_chan * ent_tank_dict['vel_capture'].to(u.m/u.s).magnitude *
-                       np.sin(ent_tank_dict['angle_plate'].to(u.rad).magnitude))))
+    B_plate = ent_tank_inputs['S_plate'] + ent_tank_inputs['thickness_plate']
+    N_plates = np.ceil(np.sqrt(q_plant/(ent_tank_inputs['B_plate'].to(u.m).magnitude
+                       * W_chan * ent_tank_inputs['vel_capture'].to(u.m/u.s).magnitude *
+                       np.sin(ent_tank_inputs['angle_plate'].to(u.rad).magnitude))))
     return N_plates
 
 @u.wraps(u.m, [u.m**3/u.s, u.m, None], False)
 def L_plate_ent_tank(q_plant, W_chan, ent_tank_inputs=ent_tank_dict):
     """Return the length of the plates in the entrance tank."""
     L_plate = ((q_plant/(num_plates_ent_tank(q_plant, W_chan) * W_chan *
-               ent_tank_dict['vel_capture'].to(u.m/u.s).magnitude *
-               np.cos(ent_tank_dict['angle_plate'].to(u.rad).magnitude)))
-               - (ent_tank_dict['S_plate'].to(u.m).magnitude *
-               np.tan(ent_tank_dict['angle_plate'].to(u.rad).magnitude)))
+               ent_tank_inputs['vel_capture'].to(u.m/u.s).magnitude *
+               np.cos(ent_tank_inputs['angle_plate'].to(u.rad).magnitude)))
+               - (ent_tank_inputs['S_plate'].to(u.m).magnitude *
+               np.tan(ent_tank_inputs['angle_plate'].to(u.rad).magnitude)))
     return L_plate
 
 @u.wraps([u.inch, None, u.m], [u.m**3/u.s, u.degK, u.m, u.m, None], False)
-def ent_tank(q_plant, temp, depth_end, W_chan, ent_tank_inputs=ent_tank_dict):
-    ND_drain = drain_OD(q_plant, temp, depth_end, ent_tank_inputs=ent_tank_dict).magnitude
-    N_plates = num_plates_ent_tank(q_plant, W_chan, ent_tank_inputs=ent_tank_dict).magnitude
-    L_plate = L_plate_ent_tank(q_plant, W_chan, ent_tank_inputs=ent_tank_dict).magnitude
-    return ND_drain, N_plates, L_plate
+def ent_tank_agg(q_plant, temp, depth_end, W_chan, ent_tank_inputs=ent_tank_dict):
+    OD_drain = drain_OD(q_plant, temp, depth_end, ent_tank_inputs).magnitude
+    N_plates = num_plates_ent_tank(q_plant, W_chan, ent_tank_inputs).magnitude
+    L_plate = L_plate_ent_tank(q_plant, W_chan, ent_tank_inputs).magnitude
+    return OD_drain, N_plates, L_plate
