e969f32ec3f0d4247dcff94589718aa2da18dedf
Ethan Keller <ethan.keller@gmail.com>
2018-08-31 09:56:20 -0400

    Bumped version to 0.0.14

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='aguaclara',
-      version='0.0.13',
+      version='0.0.14',
       description='AguaClara Infrastructure Design Engine',
       url='https://github.com/AguaClara/aguaclara_design',
       author='AguaClara at Cornell',
