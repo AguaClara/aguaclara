f34493733247a0cc72de834ea0b86bedadd5e02a
Aaron Yao-Smith <aty25@cornell.edu>
2018-10-13 15:23:38 -0400

    Put tests.core.test_drill functionality into function test_drill()

--- tests/core/test_drill.py
+++ tests/core/test_drill.py
@@ -1,6 +1,12 @@
 from aguaclara.core import drills
 
-print("Imperial drill bit diameters: \n" +
-      repr(drills.DRILL_BITS_D_IMPERIAL) + "\n")
-print("Metric drill bit diameters: \n" +
-      repr(drills.DRILL_BITS_D_METRIC) + "\n")
+
+def test_drill():
+    print(
+        "Imperial drill bit diameters: \n"
+        + repr(drills.DRILL_BITS_D_IMPERIAL) + "\n"
+    )
+    print(
+        "Metric drill bit diameters: \n"
+        + repr(drills.DRILL_BITS_D_METRIC) + "\n"
+    )
