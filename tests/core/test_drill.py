89fffc7bf3ea58baf8813d92a9c999f0bfa797cb
Aaron Yao-Smith <aty25@cornell.edu>
2018-10-13 14:48:58 -0400

    Refactor drill.py (drill bit diameter reference values)

---
+++ tests/core/test_drill.py
@@ -0,0 +1,6 @@
+from aguaclara.core import drills
+
+print("Imperial drill bit diameters: \n" +
+      repr(drills.DRILL_BITS_D_IMPERIAL) + "\n")
+print("Metric drill bit diameters: \n" +
+      repr(drills.DRILL_BITS_D_METRIC) + "\n")
