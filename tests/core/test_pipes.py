eb62e75d0271cb23dbe324b9468d5b05b3633f35
Aaron Yao-Smith <aty25@cornell.edu>
2018-10-16 14:26:41 -0400

    Fix test_pipes.py

--- tests/core/test_pipes.py
+++ tests/core/test_pipes.py
@@ -1,49 +1,22 @@
 import unittest
-import os
-
-import pandas as pd
-import numpy as np
 
 from aguaclara.core.units import unit_registry as u
 from aguaclara.core import pipes
 
 
 class TestPipes(unittest.TestCase):
-    # TODO: better test coverage (e.g. metric)
+    # TODO: better test coverage (e.g. metric, functions outside Pipe)
 
     def test_pipes(self):
         pipe = pipes.Pipe(nd=(7.0 * u.inch), sdr=35.0)
-        pipe_df = pd.read_csv(os.path.join(
-            os.path.dirname(__file__),
-            '../../aguaclara/core/data/pipe_database.csv'
-        ))
-        assert_list_almost_equal = np.vectorize(self.assertAlmostEqual)
-        assert_list_almost_equal(
-            [
-                pipe.od(),
-                pipe.id_sdr(),
-                pipe.id_sch40(),
-            ],
-            [
-                7.625 * u.inch,
-                7.1892857 * u.inch,
-                7.023 * u.inch,
-            ]
-        )
-        assert_list_almost_equal(
-            [
-                pipes.ND_all_available(),
-                pipes.ID_SDR_all_available(35.0),
-                pipes.ND_SDR_available(7.1892857 * u.inch, 35.0),
-                pipes.ND_available(4.7 * u.inch)
-            ],
-            [
-                (pipe_df[pipe_df['Used'] == 1]['NDinch'].values) * u.inch,
-                (pipe_df['NDinch'].values * 0.1142856) * u.inch,
-                7.0 * u.inch,
-                6.0 * u.inch
-            ]
-        )
+
+        def assert_quantity_equal(a, b):
+            self.assertAlmostEqual(a.magnitude, b.magnitude)
+            self.assertAlmostEqual(a.units, b.units)
+
+        assert_quantity_equal(pipe.od(), 7.625 * u.inch)
+        assert_quantity_equal(pipe.id_sdr(), 7.18928571 * u.inch)
+        assert_quantity_equal(pipe.id_sch40(), 7.023 * u.inch)
 
 
 if __name__ == '__main__':
