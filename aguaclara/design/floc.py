d0d39d49c919e8ec88d50689ca03f56be55c3352
Aaron Yao-Smith <aty25@cornell.edu>
2018-10-28 15:40:00 -0400

    Start refactoring floc.a_ent_tank

--- aguaclara/design/floc.py
+++ aguaclara/design/floc.py
@@ -1,47 +1,49 @@
 import numpy
 import aguaclara.core.constants as con
 import aguaclara.core.physchem as pc
+from aguaclara.design import ent_tank as et
 from aguaclara.core.units import unit_registry as u
 
 
 FREEBOARD = 10 * u.cm
-BLANKET_HEIGHT = 0.25 * u.m  # vertical height of floc blanket from peak of slope to weir
+# vertical height of floc blanket from peak of slope to weir
+BLANKET_HEIGHT = 0.25 * u.m
 
 # Distance that the rapid mix coupling extends into the first floc channel
 # so that the RM orifice place can be fixed in place.
 COUPLING_EXT_L = 5 * u.cm
 
-##The minor loss coefficient is 2. According to measurements at Agalteca
+# The minor loss coefficient is 2. According to measurements at Agalteca
 # and according to
 # https://confluence.cornell.edu/display/AGUACLARA/PAHO+Water+Treatment+Publications
 # (page 100 in chapter on flocculation)
 OPTION_H = 0
 
-##Increased both to provide a safety margin on flocculator head loss and
+# Increased both to provide a safety margin on flocculator head loss and
 # to simultaneously scale back on the actual collision potential we are
 # trying to achieve.
 BAFFLE_K_MINOR = 2.5
 
 BAFFLE_SET_BACK_PLASTIC_S = 2 * u.cm
 
-###Target flocculator collision potential basis of design
+# Target flocculator collision potential basis of design
 COLL_POT_BOD = 75 * u.m**(2/3)
 
-##Minimum width of flocculator channel required for constructability based
+# Minimum width of flocculator channel required for constructability based
 # on the width of the human hip
 W_MIN = 45 * u.cm
 
-##Minimum and maximum distance between expansions to baffle spacing ratio for
-#flocculator geometry that will provide optimal efficiency.
+# Minimum and maximum distance between expansions to baffle spacing ratio for
+# flocculator geometry that will provide optimal efficiency.
 HS_RATIO_MIN = 3
 HS_RATIO_MAX = 6
 
-##Ratio of the width of the gap between the baffle and the wall and the
+# Ratio of the width of the gap between the baffle and the wall and the
 # spacing between the baffles.
 BAFFLE_RATIO = 1
 
-##Max energy dissipation rate in the flocculator, basis of design.
-ENERGY_DIS_FLOC_BOD = 10* u.mW/u.kg
+# Max energy dissipation rate in the flocculator, basis of design.
+ENERGY_DIS_FLOC_BOD = 10 * u.mW/u.kg
 
 DRAIN_TIME = 15 * u.min
 
@@ -53,7 +55,7 @@ MOD_EDGE_LAST_PIPE_S = 10 * u.cm
 
 RM_RESTRAINER_ND = 0.5 * u.inch
 
-###Height that the drain stub extends above the top of the flocculator wall
+# Height that the drain stub extends above the top of the flocculator wall
 DRAIN_STUB_EXT_H = 20 * u.cm
 
 MOD_PIPE_EDGE_S = 10 * u.cm
@@ -62,10 +64,10 @@ BAFFLE_THICKNESS = 2 * u.mm
 
 BAFFLE_RIGID_H_THICKNESS = 15*u.cm  # Is this a height or a thickness?
 
-#The piping size for the main part of the floc modules
+# The piping size for the main part of the floc modules
 MODULES_MAIN_ND = (1/2)*u.inch
 
-#The diameter of the oversized cap used to assemble the floc modules
+# The diameter of the oversized cap used to assemble the floc modules
 MODULES_LARGE_ND = 1.5*u.inch
 
 
@@ -99,7 +101,6 @@ class Flocculator:
         return (pc.gravity.magnitude * self.HL) / \
                (self.GT * pc.viscosity_kinematic(self.temp).magnitude)
 
-
     def vol(self):
         """Return the total volume of the flocculator using plant flow rate, head
         loss, collision potential and temperature.
@@ -116,7 +117,6 @@ class Flocculator:
         6.233 meter3
         """
         return (self.GT * self.q) / self.vel_gradient_avg
-
 
     @u.wraps(u.cm, [u.m**3/u.s, u.m, None, u.degK, u.m], False)
     def width_HS_min(q_plant, hl, Gt, T, depth_end):
@@ -161,7 +161,6 @@ class Flocculator:
                                         * nu)) ** (1/3)) * q_plant / depth_end
         return w
 
-
     @u.wraps(u.cm, [u.m**3/u.s, u.m, None, u.degK, u.m], False)
     def width_floc_min(q_plant, hl, Gt, T, depth_end):
         """Return the minimum channel width required.
@@ -198,7 +197,6 @@ class Flocculator:
         45 centimeter
         """
         return max(width_HS_min(q_plant, hl, Gt, T, depth_end).magnitude, con.FLOC_W_MIN_CONST.magnitude)
-
 
     @u.wraps(None, [u.m**3/u.s, u.m, None, u.degK, u.m, u.m], False)
     def num_channel(q_plant, hl, Gt, T, W_tot, depth_end):
@@ -243,9 +241,7 @@ class Flocculator:
         num = np.floor(num/2)*2
         return int(max(num, 2))
 
-
-    @u.wraps(u.m**2, [u.m**3/u.s, u.m, None, u.degK, u.m], False)
-    def area_ent_tank(q_plant, hl, Gt, T, depth_end):
+    def a_ent_tank(self):
         """Return the planview area of the entrance tank given plant flow rate,
         headloss, target collision potential, design temperature, and depth of
         water at the end of the flocculator.
@@ -278,30 +274,21 @@ class Flocculator:
         >>> area_ent_tank(20*u.L/u.s, 40*u.cm, 37000, 25*u.degC, 2*u.m)
         1 meter ** 2
         """
-        # guess the planview area before starting iteration
-        A_new = 1*u.m**2
-        A_ratio = 0
+        # first guess planview area
+        a_new = 1 * u.m**2
+        a_ratio = 0
+        tolerance = 0.01
+        while a_ratio > (1 + tolerance):
+            a_et_pv = a_new
+            a_floc_pv = self.vol / (depth_end + (self.hl/2))
+            a_etf_pv = a_et_pv + a_floc_pv
 
-        while (A_ratio) > 1.01 and (A_ET_PV/A_new) < 0.99:
-            A_ET_PV = A_new
+            w_tot = a_etf_pv / opt.L_sed
+            w_chan = w_tot / self.num_channel
 
-            vol_floc = vol_floc(q_plant, hl, Gt, T)
-            A_floc_PV = vol_floc/(depth_end + hl/2)
-            A_ETF_PV = A_ET_PV + A_floc_PV
-
-            W_min = width_floc_min(q_plant, hl, Gt, T, depth_end)
-
-            W_tot = A_ETF_PV/opt.L_sed
-
-            num_chan = num_channel(q_plant, hl, Gt, T, W_tot)
-            W_chan = W_tot/num_chan
-
-            A_new = opt.L_ET_max*W_chan
-
-            A_ratio = A_new/A_ET_PV
-
+            a_new = ht.L_MAX * w_chan
+            a_ratio = a_new / a_et_pv
         return A_new.to(u.m**2).magnitude
-
 
     @u.wraps(u.m, [u.m**3/u.s, u.m, None, u.degK, u.m], False)
     def exp_dist_max(q_plant, hl, Gt, T, W_chan):
@@ -342,7 +329,6 @@ class Flocculator:
         exp_dist_max = term1*term2
         return exp_dist_max
 
-
     @u.wraps(None, [u.m**3/u.s, u.m, None, u.degK, u.m], False)
     def num_expansions(q_plant, hl, Gt, T, depth_end):
         """"Return the minimum number of expansions per baffle space.
@@ -374,7 +360,6 @@ class Flocculator:
         ???
         """
         return int(np.ceil(depth_end/(exp_dist_max(q_plant, hl, Gt, T)).magnitude))
-
 
     @u.wraps(u.m, [u.m**3/u.s, u.m, None, u.degK, u.m], False)
     def height_exp(q_plant, hl, Gt, T, depth_end):
@@ -409,7 +394,6 @@ class Flocculator:
         """
         return depth_end/num_expansions(q_plant, hl, Gt, T)
 
-
     @u.wraps(u.m, [u.m**3/u.s, u.m, None, u.degK, u.m], False)
     def baffle_spacing(q_plant, hl, Gt, T, W_chan):
         """Return the spacing between baffles based on the target velocity gradient
@@ -443,9 +427,9 @@ class Flocculator:
         ."""
         g_avg = G_avg(hl, Gt, T).magnitude
         nu = pc.viscosity_kinematic(T).magnitude
-        term1 = (K_e/(2 * exp_dist_max(q_plant, hl, Gt, T, W_chan).magnitude * (g_avg**2) * nu))**(1/3)
+        term1 = (K_e/(2 * exp_dist_max(q_plant, hl, Gt, T,
+                                       W_chan).magnitude * (g_avg**2) * nu))**(1/3)
         return term1 * q_plant/W_chan
-
 
     @u.wraps(None, [u.m**3/u.s, u.m, None, u.degK, u.m, u.m, u.m], False)
     def num_baffles(q_plant, hl, Gt, T, W_chan, L, baffle_thickness):
@@ -487,7 +471,8 @@ class Flocculator:
         >>> num_baffles(20*u.L/u.s, 20*u.cm, 37000, 25*u.degC, 2*u.m, 2*u.m, 21*u.m)
         -1
         """
-        num = round((L / (baffle_spacing(q_plant, hl, Gt, T, W_chan).magnitude + baffle_thickness)))
+        num = round(
+            (L / (baffle_spacing(q_plant, hl, Gt, T, W_chan).magnitude + baffle_thickness)))
         # the one is subtracted because the equation for num gives the number of
         # baffle spaces and there is always one less baffle than baffle spaces due
         # to geometry
