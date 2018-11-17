1934be8ec90ec20ae5108e50cf8adb6553577d81
Oliver Leung <oal22@cornell.edu>
2018-11-17 16:05:14 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='aguaclara',
-      version='0.0.14',
+      version='0.0.16',
       description='AguaClara Infrastructure Design Engine',
       url='https://github.com/AguaClara/aguaclara_design',
       author='AguaClara at Cornell',
