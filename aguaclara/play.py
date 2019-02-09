738418d45aaa379b6063725afde2fa43bf2b4038
Oliver Leung <oal22@cornell.edu>
2019-02-09 17:14:01 -0500

    Clean up imports and setup functions

--- aguaclara/play.py
+++ aguaclara/play.py
@@ -1,36 +1,18 @@
-"""This module is intended to provide easy set up for a playground development
-environment using the aguaclara package.
-
-It imports all commonly used packages with one line, ensures Python is
-run in the correct virtual environment, sets sig figs correctly and provides
-any additional environment massaging to get to designing as quickly as
-possible. This should NOT be used by other modules within aguaclara as it
-results in unnecessary imports.
+"""
+Provides easy setup for using the aguaclara package and all of its modules.
 
 Usage:
-
-    * Import all into your global namespace with: `from aguaclara.play *`
-    * setup_aide() should run during import.
-
-Now you should be able to execute:
-    *`np.array([1,2,3,4])
-And your numbers should be limited to four significant figures when printed.
-
+    `from aguaclara.play import *`
 """
 
-# Third-party imports
 import math
 import numpy as np
 import pandas as pd
 import matplotlib
+import matplotlib.style
 import matplotlib.pyplot as plt
+import warnings
 
-# Design imports
-# from aguaclara.design.lfom import LFOM    #NOTE: temporarily disabled for release 0.0.15 to prevent problems
-                                            #with lacking Onshape set up (unnecessary for research teams)
-
-
-# Core imports
 import aguaclara
 import aguaclara.core.pipes as pipe
 from aguaclara.core.units import unit_registry as u
@@ -40,24 +22,11 @@ import aguaclara.core.materials as mat
 import aguaclara.core.utility as ut
 import aguaclara.core.head_loss as k
 import aguaclara.core.pipeline as pipeline
-# import aguaclara.core.optional_inputs as opt
-import warnings
 
+# Temporarily disabled for release 0.0.15 to prevent problems with lacking
+# Onshapepy setup. Re-enable when Onshapepy backend has been resolved.
+# from aguaclara.design.lfom import LFOM
 
-# deprecated imports
-# import aguaclara.core.expert_inputs as exp
-
-
-def setup_aide():
-    """
-    This is the public function that should be called to completely setup the aide environment
-    in a jupyter notebook.
-    :return:
-    """
-    matplotlib.style.use('ggplot')
-    set_sig_fig()
-    #ensure_in_a_virtual_environment()
-        # Don't want to scare the kiddos with virtual environments yet! TODO.
 
 def set_sig_fig(n: int = 4):
     """Set the default number of significant figures used to print pint, pandas and numpy values
@@ -86,16 +55,5 @@ def set_sig_fig(n: int = 4):
     pd.options.display.float_format = ('{:,.' + str(n) + '}').format
 
 
-def ensure_in_a_virtual_environment():
-    """
-    Warn users if not in a virtual environment
-    """
-    import sys
-    # way to test for virtual environment: https://stackoverflow.com/a/1883251/5136799
-    if hasattr(sys, 'real_prefix'):
-        raise UserWarning("aguaclara should always be run in a virtual environment to ensure"
-                          "that all dependencies are correctly installed. Please refer to the"
-                          "readme for virtual environment setup instructions.")
-
-# Run when imported
-setup_aide()
+set_sig_fig()
+matplotlib.style.use('ggplot')
