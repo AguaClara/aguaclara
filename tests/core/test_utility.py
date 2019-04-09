b04b86c83df2df0d53ac6c48d7b1238d9b97fa9f
Swastik Chaki <sc2267@cornell.edu>
2019-04-09 15:10:39 -0400

    Tests for max and min functions

--- tests/core/test_utility.py
+++ tests/core/test_utility.py
@@ -11,3 +11,12 @@ class UtilityTest(unittest.TestCase):
         self.assertAlmostEqual(ut.round_sf(-456.789 * u.L/u.s, 4), -456.8 * u.L/u.s)
         self.assertAlmostEqual(ut.round_sf(0, 4), 0)
         self.assertAlmostEqual(ut.round_sf(0 * u.m, 4), 0 * u.m)
+    def test_max(self):
+        self.assertEquals(ut.max(2 * u.m, 4 * u.m),4 * u.m)
+        self.assertEquals(ut.max(3 * u.m, 1 * u.m, 6 * u.m, 10 * u.m, 1.5 * u.m), 10 * u.m)
+        self.assertEquals(ut.max(2 * u.m),2 * u.m)
+
+    def test_min(self):
+        self.assertEquals(ut.min(2 * u.m, 4 * u.m), 2 * u.m)
+        self.assertEquals(ut.min(3 * u.m, 1 * u.m, 6 * u.m, 10 * u.m, 1.5 * u.m), 1 * u.m)
+        self.assertEquals(ut.min(2 * u.m), 2 * u.m)
