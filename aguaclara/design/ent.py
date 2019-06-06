98bb2ebdda86d4f50d28688afcc5ab71a58d99ad
Oliver Anthony Leung <oal22@cornell.edu>
2019-06-06 15:51:09 -0400

    Small style fixes

--- aguaclara/design/ent.py
+++ aguaclara/design/ent.py
@@ -5,68 +5,21 @@
 #. introduces chemical dosing through the CDC <add link> using the water level set by the LFOM <add link>.
 
 Example:
-    >>> from aguaclara.design.ent_tank import * #Maybe we shouldn't do this to avoid bringing other variables into scope? -Oliver L., oal22, 5 Jun 2019
+    >>> from aguaclara.design.ent_tank import *
     >>> ent_tank = EntranceTank(q = 20 * u.L / u.s, floc_chan_w = 42.0 * u.inch,...)
     >>> ent_tank.plate_n
     15
 """
-from aguaclara.core.units import unit_registry as u
-import aguaclara.core.physchem as pc
 import aguaclara.core.constants as con
-import aguaclara.core.materials as mat
-import aguaclara.core.pipes as pipe
 import aguaclara.core.head_loss as hl
+import aguaclara.core.materials as mat
+import aguaclara.core.physchem as pc
+import aguaclara.core.pipes as pipe
+from aguaclara.core.units import unit_registry as u
 import aguaclara.core.utility as ut
 
 import numpy as np
 
-L_MAX = 2.2 * u.m
-
-# Angle of the sloped walls of the entrance tank hoppers
-ENT_TANK_SLOPE_ANGLE = 45 * u.deg
-
-# Extra space around the float (increase in effective diameter) to
-# ensure that float has free travel
-FLOAT_S = 5 * u.cm
-HOPPER_PEAK_W = 3 * u.cm
-MOD_ND = 0.5 * u.inch
-
-# Distance from the front wall to the pipe stubs in the hopper drains so
-# that an operator can easily reach them.
-WALL_DRAIN_DIST_MAX = 40 * u.cm
-MOD_SPACER_ND = 0.75 * u.inch
-
-# Thickness of the PVC disk used as the float for the chemical dose
-# controller lever arm.
-FLOAT_THICKNESS = 5 * u.cm
-LAMINA_PIPE_EDGE_S = 5 * u.cm
-
-# Nom diam of the pipes that are embedded in the entrance tank slope
-# to support the plate settler module
-PLATE_SUPPORT_ND = 3 * u.inch
-
-# Increased to get better mixing (10/10/2015 by Monroe)
-RAPID_MIX_EDR = 3 * u.W / u.kg
-RAPID_MIX_PLATE_RESTRAINER_ND = 0.5 * u.inch
-FLOAT_ND = 8 * u.inch
-
-# Are these next two variables supposed to be an expert input/calculated value?
-# -Oliver (oal22), 4 Jun 19
-
-# Minimum pipe size to handle grit and to ensure that the pipe can be
-# easily unclogged
-DRAIN_MIN_ND = 3 * u.inch
-DRAIN_ND = 3 * u.inch  # This is constant for now
-
-REMOVABLE_WALL_THICKNESS = 5*u.cm
-
-# Parameters are arbitrary - need to be calculated
-REMOVABLE_WALL_SUPPORT_H = 4 * u.cm
-REMOVABLE_WALL_SUPPORT_THICKNESS = 5 * u.cm
-HOPPER_LEDGE_THICKNESS = 15 * u.cm
-WALKWAY_W = 1 * u.m
-RAPID_MIX_ORIFICE_PLATE_THICKNESS = 2 * u.cm
-RAPID_MIX_AIR_RELEASE_ND = 1 * u.inch
 
 class EntranceTank(object): 
     """Design an AguaClara plant's entrance tank.
@@ -106,7 +59,7 @@ class EntranceTank(object):
                  floc_end_depth=2.0 * u.m,
                  plate_s=2.5 * u.cm,
                  plate_thickness=2.0 * u.mm,
-                 plate_angle = 60.0 * u.deg,
+                 plate_angle = 50.0 * u.deg,
                  plate_capture_vel = 8.0 * u.mm / u.s,
                  fab_space=5.0 * u.cm,
                  temp=20.0 * u.degC,
@@ -175,7 +128,7 @@ class EntranceTank(object):
 
     @property
     def l(self):
-        """The length of the tank."""
+        """The length of the entrance tank."""
         plate_array_thickness = \
             (self.plate_thickness * self.plate_n) + \
             (self.plate_s * (self.plate_n - 1))
@@ -188,4 +141,41 @@ class EntranceTank(object):
             (self.plate_l * np.cos(self.plate_angle.to(u.rad))) + \
             (self.lfom_nd * 2)
         return l
-        
+
+# Angle of the sloped walls of the entrance tank hoppers
+ENT_TANK_SLOPE_ANGLE = 45 * u.deg
+
+# Extra space around the float (increase in effective diameter) to
+# ensure that float has free travel
+FLOAT_S = 5 * u.cm
+HOPPER_PEAK_W = 3 * u.cm
+MOD_ND = 0.5 * u.inch
+
+# Distance from the front wall to the pipe stubs in the hopper drains so
+# that an operator can easily reach them.
+WALL_DRAIN_DIST_MAX = 40 * u.cm
+MOD_SPACER_ND = 0.75 * u.inch
+
+# Thickness of the PVC disk used as the float for the chemical dose
+# controller lever arm.
+FLOAT_THICKNESS = 5 * u.cm
+LAMINA_PIPE_EDGE_S = 5 * u.cm
+
+# Nom diam of the pipes that are embedded in the entrance tank slope
+# to support the plate settler module
+PLATE_SUPPORT_ND = 3 * u.inch
+
+# Increased to get better mixing (10/10/2015 by Monroe)
+RAPID_MIX_EDR = 3 * u.W / u.kg
+RAPID_MIX_PLATE_RESTRAINER_ND = 0.5 * u.inch
+FLOAT_ND = 8 * u.inch
+
+REMOVABLE_WALL_THICKNESS = 5*u.cm
+
+# Parameters are arbitrary - need to be calculated
+REMOVABLE_WALL_SUPPORT_H = 4 * u.cm
+REMOVABLE_WALL_SUPPORT_THICKNESS = 5 * u.cm
+HOPPER_LEDGE_THICKNESS = 15 * u.cm
+WALKWAY_W = 1 * u.m
+RAPID_MIX_ORIFICE_PLATE_THICKNESS = 2 * u.cm
+RAPID_MIX_AIR_RELEASE_ND = 1 * u.inch
