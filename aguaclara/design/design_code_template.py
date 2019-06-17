614a0cd0267f46e346bbb3b1d481c8a35c975c78
Ryan Richardson <rrr225@cornell.edu>
2019-06-17 13:02:36 -0400

    Finished Monroe/pc design code template

--- aguaclara/design/design_code_template.py
+++ aguaclara/design/design_code_template.py
@@ -1,7 +1,8 @@
 """Welcome to the design code template. This file is meant to teach you how to 
 write AIDE Design Code.
 
-These are the imports from other files or python packages that you would want to use in the file
+These are the imports from other files or python packages that you would want 
+to use in the file.
 """
 import aguaclara.core.constants as con
 import aguaclara.core.drills as drill
@@ -16,6 +17,16 @@ from aguaclara.design.component import Component
 
 import numpy as np
 
+# Naming convention for functions, variables, etc:
+# The naming convention is like a tree. To write the variable version for the 
+# minimum area of a pipe in the LFOM class, you start with the most genera, 
+# branch, pipe, then a branch (or property) of the pipe, area, then a branch 
+# of area, minimum. So, the var name would be pipe_area_minimum. This is overly 
+# long however so we have the design variable naming conventions google sheet, 
+# to learn how to abbreviate each word. Using the google sheet, the variable 
+# name for the minimum area of a pipe is pipe_a_min. Other things to remember, 
+# is everything is lowercase and underscores other than classes (capitalize 
+# first letter, no spaces) and class variables(all caps, underscores)
 
 class PressureCooker(Component):
 	"""Design Monroe's pressure cooker.
@@ -29,9 +40,9 @@ class PressureCooker(Component):
 	Design Inputs:
 	- ``q (float * u.L / u.s)``: Flow rate (recommended, defaults to 20L/s).
 	- ``temp (float * u.degC)``: Water Temperature (recommended, defaults to 
-	20 degrees celsius).
-	- ``rice_and_beans_ratio (float)``: The Monratio (optional, defaults to 
-	20L/s).
+	20°C).  
+	- ``rice_and_beans_ratio (float)``: The Monratio (optional, defaults to
+	0.5).
 	- ``<name of input> (<type>)``: <Description> (<default>)
 	"""
 	def __init__(self, q = 20.0 * u.L/u.s, temp = 20.0 * u.degC,
@@ -42,11 +53,25 @@ class PressureCooker(Component):
 		# same q and temp. In later functions, you can use the q and temp by 
 		# writing self.q and self.temp.
 		super().__init__(q = q, temp = temp)
-		
+
+		# The line below instantiates a property to a PressureCooker object, 
+		# allows you from now on to use rice_and_beans_ratio in later functions
+		# through self.rice_and_beans_ratio.
 		self.rice_and_beans_ratio = rice_and_beans_ratio
 
+	# This is the format for writing methods, use spaces around operators, 
+	# minimal parentheses, as well as refrain from having the whole body being 
+	# wrapped in a return. Also, uses very straight to the point documentation, 
+	# following the form below.
 	def rb_per_person(self, people_n, time_to_eat):
-		rb_per_person = self.q * self.rice_and_beans_ratio * time_to_eat/ people_n
+		"""The rice and beans per person.
+		
+		Args:
+			- ``people_n (float)``
+			- ``time_to_eat (float * u.s)``
+		"""
+		rb_per_person = self.q * self.rice_and_beans_ratio * time_to_eat / \
+			people_n
 		return rb_per_person
 
 # Like other component classes, the Monroe class must extend the Component class
@@ -92,19 +117,39 @@ class Monroe(Component):
 		# writing self.q and self.temp.
 		super().__init__(q = q, temp = temp)
 
-		
+		# The lines below instantiates a property to a Monroe object. In later 
+		# functions, you can use the rice_and_beans_eaten and pc, by writing 
+		# self.<property name>.
 		self.rice_and_beans_eaten = rice_and_beans_eaten
 		self.pc = pc
 
+		# If your component has a subcomponent (self.pc for our case, as it's default 
+		# value is an object), the line below propogates the configurations to 
+		# all of the classes subcomponents (monroe's pc will have the same 
+		# q and temp). You must however put these subcomponents in a list, 
+		# even if it's one subcomponent 
 		super().propogate_config([self.pc])
 
+	# Many properties of a component need some calculation, this is too much to 
+	# put in an __init__ function. So we make functions with only the self 
+	# parameter(you can't add more) and type @property. This is essentially a 
+	# normal class function except it is called as if it was a property. 
+	# For example, to call h, it is self.h. Use @property functions whenever 
+	# you can.
+	# The format for writing methods:
+	# Use spaces around operators, minimal parentheses, keep lines under 80 
+	# characters (use \ if necessary),as well as refrain from having the whole
+	# body being wrapped in a return. Also, uses very straight to the point
+	# documentation in the same form as the one below.
     @property
     def h(self):
+		"""The height."""
 		h = self.q * (0.06414368184 * u.s / u.m ** 2)
 		return h.to(u.ft)
-	
+
     @property
     def love_for_sustainable_design(self):
-        love_for_sustainable_design = self.BEAN_DENSITY * \
-            self.rice_and_beans_eaten
-        return love_for_sustainable_design.to(u.kg)
+		"""His love for sustainable design."""
+		love_for_sustainable_design = self.BEAN_DENSITY * \
+			self.rice_and_beans_eaten
+		return love_for_sustainable_design.to(u.kg)
