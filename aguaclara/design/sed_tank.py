7e3764a34c8df1d55865e5143c4638ba015c27e1
Ryan Richardson <rrr225@cornell.edu>
2019-06-24 10:47:09 -0400

    beggining stages of sed tank bay
    Co-authored-by: Oliver Leung <oal22@cornell.edu>

--- aguaclara/design/sed_tank.py
+++ aguaclara/design/sed_tank.py
@@ -13,7 +13,7 @@ from aguaclara.design.component import Component
 import numpy as np
 import math
 
-from onshapepy import Part
+# from onshapepy import Part
 
 
 class SedimentationTank(Component):
@@ -23,13 +23,15 @@ class SedimentationTank(Component):
         >>> sed_tank_bay = SedimentationTankBay()
     """
     def __init__(self,
-                 plant_q=20.0 * u.L / u.s,
+                 q=20.0 * u.L / u.s,
+                 temp=20.0 * u.degC,
                  vel_upflow=1.0 * u.mm / u.s,
                  l_inner=5.8 * u.m,
                  w_inner=42.0 * u.inch,
-                 diffuser_vel_max=442.9 * u.mm / u.s,
+                 diffuser_vel_max=44.29 * u.cm / u.s,
                  diffuser_n=108,
                  diffuser_wall_thickness=1.17 * u.inch,
+                 inlet_hl=1 * u.cm,
                  plate_settler_angle=60.0 * u.deg,
                  plate_settler_s=2.5 * u.cm,
                  plate_settler_thickness=2.0 * u.mm,
@@ -39,7 +41,7 @@ class SedimentationTank(Component):
                  exit_man_orifice_n=58,
                  exit_man_orifice_q_ratio_max=0.8,
                  
-                 plate_w=41.0 * u.inch):
+                 w=41.0 * u.inch):
         """Instantiates a SedimentationTankBay with the specified values.
 
         All args are optional, and default to values that are optimized for a 20
@@ -73,7 +75,7 @@ class SedimentationTank(Component):
         Returns:
              SedimentationTankBay object.
         """
-        self.plant_q = plant_q
+        super().__init__(q = q, temp = temp)
         self.vel_upflow = vel_upflow
         self.l_inner = l_inner
         self.w_inner = w_inner
@@ -89,14 +91,17 @@ class SedimentationTank(Component):
         self.exit_man_orifice_q_ratio_max = exit_man_orifice_q_ratio_max
         self.diffuser_wall_thickness = diffuser_wall_thickness
 
+        self.w = w
+        self.inlet_hl = inlet_hl
+    
     @property
-    def q(self):
+    def q_tank(self):
         """
         Returns:
-            Flow rate in a sedimentation tank bay (float * u.L / u.s).
+            Flow rate in a sedimentation tank (float * u.L / u.s).
         """
-        q = self.l_inner * self.w_inner * self.vel_upflow
-        return q.to(u.L / u.s)
+        q_tank = self.l_inner * self.w_inner * self.vel_upflow
+        return q_tank.to(u.L / u.s)
 
     @property
     def n(self):
@@ -108,14 +113,36 @@ class SedimentationTank(Component):
         return int(n)
 
     @property
-    def w_diffuser_inner_min(self):
+    def diffuser_hl(self):
+        return self.inlet_hl / self.diffuser_n
+    
+    @property
+    def diffuser_vel(self):
+        diffuser_vel = np.sqrt(2 * con.GRAVITY * self.diffuser_hl)
+        return diffuser_vel.to(u.mm / u.s)
+        
+    @property
+    def diffuser_w_inner(self):
+        diffuser_w_inner = self.w * self.vel_upflow / self.diffuser_vel
+        return diffuser_w_inner.to(u.cm)
+
+    @property
+    def diffuser_a(self):
         """
-        Returns:
-            Minimum inner width of each diffuser in the sedimentation tank (float).
+        Calculates manifold diffuser area from flow rate.
         """
-        return ((self.vel_upflow.to(u.inch/u.s).magnitude /
-                 self.diffuser_vel_max.to(u.inch / u.s).magnitude)
-                * self.w_inner)
+        diffuser_a = self.q / (self.diffuser_vel * self.diffuser_n)
+        return diffuser_a
+    
+    # @property
+    # def w_diffuser_inner_min(self):
+    #     """
+    #     Returns:
+    #         Minimum inner width of each diffuser in the sedimentation tank (float).
+    #     """
+    #     return ((self.vel_upflow.to(u.inch/u.s).magnitude /
+    #              self.diffuser_vel_max.to(u.inch / u.s).magnitude)
+    #             * self.w_inner)
 
     # Note: we need to specify in Onshape a 15% stretch difference between
     # the circumference of both diffuser ends' inner/outer circumferences.
@@ -197,10 +224,4 @@ class SedimentationTank(Component):
                      ).to(u.m)
         return L_sed_plate
 
-    @property
-    def diffuser_a(self):
-        """
-        Calculates manifold diffuser area from flow rate.
-        """
-        diffuser_a = self.q / (self.diffuser_vel_max * self.diffuser_n)
-        return diffuser_a
+
