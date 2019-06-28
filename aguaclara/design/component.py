3fc9a547ebf725c41c21c501e51560dfb8eab3b8
Oliver Leung <oal22@cornell.edu>
2019-06-28 11:54:52 -0400

    Document component

--- aguaclara/design/component.py
+++ aguaclara/design/component.py
@@ -1,27 +1,18 @@
-"""This module provides compositional component classes the ability to propogate
-plant-wide design inputs throughout all of its subcomponents.
+"""This module allows plant component classes to
 
-Since it becomes tedious to pass said plant-wide design inputs manually to each
-subcomponent, this module creates a cache-like data structure that allows a
-component and all of its subcomponents to access a set of shared inputs.
+#. specify expert inputs and their defaults automatically without the need for an ``__init__()`` function
+#. propogate plant-wide design inputs throughout all of its subcomponents.
 
 Example:
-    from aguaclara.design.component import *
+    from aguaclara.design.component import Component
     
     class SubComponent(Component):
-        def __init__(self, q=20 * u.L / u.s, temp=20 * u.degC,
-                     h=3 * u.m):
-            super().__init__(q = q, hl = hl)
-            self.h = h
+        h = 3 * u.m
 
     class MainComponent(Component):
-        def __init__(self, q=20 * u.L / u.s, temp=20 * u.degC,
-                     l=2 * u.m,
-                     sub=SubComponent()):
-            super().__init__(q = q, hl = hl)
-            self.l = l
-
-            super().propogate_config([self.sub])
+        l = 2 * u.m
+        sub = SubComponent()
+        subcomponents = [sub]
 """
 from aguaclara.core.units import unit_registry as u
 import aguaclara.core.utility as ut
@@ -38,42 +29,54 @@ class Component(object):
     plant design variables for a component and all of its subcomponents.
 
     Args:
-        - ``pi (PlantInput)``: The shared plant design inputs for a component
-          object
+        - ``q (float * u.L / u.s)``: Flow rate (recommended, defaults to 20L/s)
+        - ``temp (float * u.degC)``: Water temperature (recommended, defaults to
+          20°C)
     """
     q = 20 * u.L / u.s
     temp = 20 * u.degC
     subcomponents = []
 
     def __init__(self, **kwargs):
+        # Update the Component object with new expert inputs, if any were given.
         self.__dict__.update(kwargs)
         
+        # Send plant-wide inputs to all subcomponents
         for subcomp in self.subcomponents:
             subcomp.q = self.q
             subcomp.temp = self.temp
         
     def serialize_properties(self):
         """Convert the properties (fields and ``@property`` functions) of a 
-        component into a dictionary.
+        component into a dictionary string.
         """
         properties = {}
-        built_in_properties = [
+        ignored_properties = [
             '__dict__',
             '__doc__',
             '__module__',
             '__weakref__',
             'subcomponents'
         ]
+        # Get all of the object's fields
         for var_name in dir(self):
             value = getattr(self, var_name)
+
+            # Serialize subcomponents
             if isinstance(value, Component):
                 properties[var_name] = value.serialize_properties()
-            elif not callable(value) and var_name not in built_in_properties:
+
+            # Serialize non-component properties
+            elif not callable(value) and var_name not in ignored_properties:
+
+                # Serialize lists or arrays
                 try: 
                     if type(value.magnitude) is np.ndarray:
                         properties[var_name] = ut.array_qtys_to_strs(value)
                     else:
                         properties[var_name] = str(value)
+                
+                # Serialize non-iterable properties
                 except: 
                     properties[var_name] = str(value)
     
