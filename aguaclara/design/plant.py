c1e12c37c1680e6b2bab0e8748c21049445d3235
Ryan Richardson <rrr225@cornell.edu>
2019-07-11 12:02:26 -0400

    Okay Sedimentor is actually ACTUALLY done

--- aguaclara/design/plant.py
+++ aguaclara/design/plant.py
@@ -1,5 +1,7 @@
-from aguaclara.design.ent_floc import *
-
+from aguaclara.design.ent_floc import EntTankFloc
+from aguaclara.design.sed import Sedimentor
+from aguaclara.design.component import Component
+from aguaclara.core.units import unit_registry as u
 
 class Plant(Component):
     """Functions for designing an AguaClara water treatment plant."""
@@ -7,14 +9,6 @@ class Plant(Component):
     q=20 * u.L/u.s
     temp=25 * u.degC
     etf = EntTankFloc()
-    subcomponents = [etf]
-    """Initialize a Plant object that represents a real AguaClara water
-    treatment plant.
-
-    :param q: Flow rate of water through the plant.
-    :type q: float * u.L/u.s
-    :param temp: Temperature of water through the plant.
-    :type q: float * u.degC
-    :returns: object
-    :rtype: Plant
-    """
+    sed = Sedimentor()
+    subcomponents = [etf, sed]
+    
