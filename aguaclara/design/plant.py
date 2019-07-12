94f742af9f3d031ebaee25e109d74247c9673236
Ryan Richardson <rrr225@cornell.edu>
2019-07-12 09:58:31 -0400

    more imports added to plant.py

--- aguaclara/design/plant.py
+++ aguaclara/design/plant.py
@@ -1,6 +1,14 @@
-from aguaclara.design.ent_floc import EntTankFloc
-from aguaclara.design.sed import Sedimentor
 from aguaclara.design.component import Component
+
+from aguaclara.design.ent_floc import EntTankFloc
+from aguaclara.design.floc import Flocculator
+from aguaclara.design.ent import EntranceTank
+from aguaclara.design.lfom import LFOM
+
+from aguaclara.design.sed import Sedimentor
+from aguaclara.design.sed_tank import SedimentationTank
+from aguaclara.design.sed_chan import SedimentationChannel
+
 from aguaclara.core.units import unit_registry as u
 
 class Plant(Component):
