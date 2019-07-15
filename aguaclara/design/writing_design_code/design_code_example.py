ef3da53034c8ba5f2130c8e8403f2afa16f21e06
Oliver Leung <oal22@cornell.edu>
2019-07-15 10:21:56 -0400

    Add changes
    Co-authored-by: ryan26192 <stevie26192@gmail.com>

--- aguaclara/design/writing_design_code/design_code_example.py
+++ aguaclara/design/writing_design_code/design_code_example.py
@@ -15,34 +15,35 @@ from aguaclara.design.component import Component
 
 
 class PressureCooker(Component):
-	"""Design Monroe's pressure cooker.
+    """Design Monroe's pressure cooker.
 
-	In order for Monroe to cook his favorite food, he must use his trusty 
-	flow rate pressure cooker!
+    In order for Monroe to cook his favorite food, he must use his trusty 
+    flow rate pressure cooker!
 
-	Design Inputs:
-	- ``q (float * u.L / u.s)``: Flow rate (recommended, defaults to 20L/s).
-	- ``temp (float * u.degC)``: Water temperature (recommended, defaults to
+    Design Inputs:
+    - ``q (float * u.L / u.s)``: Flow rate (recommended, defaults to 20L/s).
+    - ``temp (float * u.degC)``: Water temperature (recommended, defaults to
       20°C)
     - ``rice_and_beans_ratio (float)``: Ratio of rice to beans (optional,
       defaults to 0.5)
-	"""
-	def __init__(self, q=20.0 * u.L/u.s, temp=20.0 * u.degC,
-				 rice_and_beans_ratio=0.5):
-		super().__init__(q = q, temp = temp)
+    """
+    def __init__(self, **kwargs):
+        self.q = 20.0 * u.L/u.s
+        self.temp = 20.0 * u.degC
+        self.rice_and_beans_ratio = 0.5
+        
+        super().__init__(**kwargs)
 
-		self.rice_and_beans_ratio = rice_and_beans_ratio
-
-	def rb_per_person(self, people_n, time_to_eat):
-		"""The amount of rice and beans per person (L).
-		
-		Args:
-			- ``people_n (float)``: The number of people eating
-			- ``time_to_eat (float * u.s)``: The amount of time it takes to eat
-		"""
-		rb_per_person = self.q * self.rice_and_beans_ratio * time_to_eat / \
-			people_n
-		return rb_per_person
+    def rb_per_person(self, people_n, time_to_eat):
+        """The amount of rice and beans per person (L).
+        
+        Args:
+            - ``people_n (float)``: The number of people eating
+            - ``time_to_eat (float * u.s)``: The amount of time it takes to eat
+        """
+        rb_per_person = self.q * self.rice_and_beans_ratio * time_to_eat / \
+            people_n
+        return rb_per_person
 
 
 class Monroe(Component): 
@@ -66,16 +67,15 @@ class Monroe(Component):
     """
     BEAN_DENSITY = 4.0 * u.g / u.L
 
-    def __init__(self, q = 20.0 * u.L/u.s, temp = 20.0 * u.degC,
-                 rice_and_beans_eaten = 5.0 * u.L,
-                 pc = PressureCooker()):
+    def __init__(self, **kwargs):
+        self.q = 20.0 * u.L/u.s
+        self.temp = 20.0 * u.degC
+        self.rice_and_beans_eaten = 5.0 * u.L
+        self.pc = PressureCooker()
+        self.subcomponents = [self.pc]
 
-        super().__init__(q = q, temp = temp)
-
-        self.rice_and_beans_eaten = rice_and_beans_eaten
-        self.pc = pc
-
-        super().propogate_config([self.pc])
+        super().__init__(**kwargs)
+        super().set_subcomponents()
 
     @property
     def h(self):
@@ -87,5 +87,5 @@ class Monroe(Component):
     def love_for_sustainable_design(self, love_factor):
         """His love for sustainable design (kg)."""
         love_for_sustainable_design = self.BEAN_DENSITY * \
-			self.rice_and_beans_eaten * love_factor
+            self.rice_and_beans_eaten * love_factor
         return love_for_sustainable_design.to(u.kg)
