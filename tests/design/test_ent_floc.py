a703f3a98ceee84d540092610ace2b7c50c10f8e
Oliver Leung <oal22@cornell.edu>
2019-07-15 16:56:21 -0400

    Fix test

--- tests/design/test_ent_floc.py
+++ tests/design/test_ent_floc.py
@@ -1,5 +1,6 @@
 from aguaclara.design.ent_floc import EntTankFloc
 from aguaclara.core.units import unit_registry as u
+import aguaclara.core.utility as ut
 
 import pytest
 
@@ -8,7 +9,7 @@ etf_60 = EntTankFloc(q = 60.0 * u.L / u.s)
 
 @pytest.mark.parametrize('actual, expected', [
     (etf_20.ent.l, 37.6732811402825 * u.inch),
-	(etf_60.ent.l, 59.945577185244304 * u.inch),
+	(etf_60.ent.l, 59.94557718524431 * u.inch),
 
 	(etf_20.floc.chan_w, 0.45 * u.m),
     (etf_60.floc.chan_w, 0.4377982708968249 * u.m)
