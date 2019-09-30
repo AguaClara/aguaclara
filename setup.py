4d5c3fe66e7c0e11b1901b62a693da6a0aa18430
Hannah Si <35944490+HannahSi@users.noreply.github.com>
2019-09-30 11:34:23 -0400

    Fixes to typos and procoda_parser parameters (#250)
    * Fix typos in procoda_parser and units, simplify imports
    
    * Fix last row index in procoda_parser

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'aguaclara',
-    version = '0.1.11',
+    version = '0.1.12',
     description = (
         'An open-source Python package for designing and performing research '
         'on AguaClara water treatment plants.'
