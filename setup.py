484ba9efd099070847a7efa360322ce7bb20f110
Alice Zhao <41217176+alicezha0@users.noreply.github.com>
2020-01-25 20:34:47 -0500

    change matplotlib version (#262)
    * change matplotlib version
    
    * updated version; removed comment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'aguaclara',
-    version = '0.1.15',
+    version = '0.1.16',
     description = (
         'An open-source Python package for designing and performing research '
         'on AguaClara water treatment plants.'
