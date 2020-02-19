996316646e1cbebed9a34fd47a56760a34703e39
Alice Zhao <41217176+alicezha0@users.noreply.github.com>
2020-02-19 10:48:39 -0500

    Az297/matplotlib (#264)
    * change matplotlib version
    
    * updated version; removed comment
    
    * updated matplotlib version
    
    * updated pipfile.lock
    
    * Removed Python 3.5 and updated version
    
    * updated .travis.yml
    
    Co-authored-by: Oliver Leung <oal22@cornell.edu>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'aguaclara',
-    version = '0.1.16',
+    version = '0.2.0',
     description = (
         'An open-source Python package for designing and performing research '
         'on AguaClara water treatment plants.'
@@ -15,7 +15,7 @@ setup(
 
     # Ensure that this matches Pipfile > [packages]
     install_requires = [
-        'matplotlib==3.0.3', # Supports Python 3.5
+        'matplotlib',
         'urllib3',
         'pint==0.8.1',
         'pandas',
