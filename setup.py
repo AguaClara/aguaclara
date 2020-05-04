b3faa4bf0923ba1a0d1ba4bdbb2c7e7008fb6fcc
KevinSarmiento <36485127+KevinSarmiento@users.noreply.github.com>
2020-05-03 23:11:55 -0400

    Update pipe_database.csv (#270)
    * Update pipe_database.csv
    
    Updated wall thickness for ND 12" to 20"
    
    * Increment version to 0.2.1
    
    Co-authored-by: Oliver Leung <oal22@cornell.edu>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'aguaclara',
-    version = '0.2.0',
+    version = '0.2.1',
     description = (
         'An open-source Python package for designing and performing research '
         'on AguaClara water treatment plants.'
