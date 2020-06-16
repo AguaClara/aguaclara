c329388ae4546a196732135ab50e84e9a7bcbf37
Monroe Weber-Shirk <monroews@gmail.com>
2020-06-16 13:39:02 -0400

    change version to 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'aguaclara',
-    version = '0.2.1',
+    version = '0.2.2',
     description = (
         'An open-source Python package for designing and performing research '
         'on AguaClara water treatment plants.'
