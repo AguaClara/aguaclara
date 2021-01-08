9311a0ebc59161e3883a7f1df844d54e415cb61f
Fletcher Chapin <fletchapin@gmail.com>
2021-01-08 13:50:05 -0800

    Forgot to add 'process' to fields to parse

--- tests/core/test_onshape_parser.py
+++ tests/core/test_onshape_parser.py
@@ -115,7 +115,7 @@ class OnshapeParserTest(unittest.TestCase):
         link = 'https://cad.onshape.com/documents/c3a8ce032e33ebe875b9aab4/v/dc76b3f674d3d5d4f6237f35/e/d75b2f7a41dde39791b154e8'
         measurements, templates, processes = parse.get_parsed_measurements(
             link,
-            fields=['variables', 'template'],
+            fields=['variables', 'template', 'process'],
             for_docs=False
         )
 
