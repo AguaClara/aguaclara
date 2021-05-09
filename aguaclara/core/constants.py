bd2715a63be533d5f380495f326d32c7cccd7fb5
vaishnavi-vinodh <63830657+vaishnavi-vinodh@users.noreply.github.com>
2021-05-09 10:27:19 -0400

    Update constants.py
    Updating Constants.py
    - Got rid of deprecated fields/constants

--- aguaclara/core/constants.py
+++ aguaclara/core/constants.py
@@ -5,32 +5,6 @@ from aguaclara.core.units import u
 
 # NOTE: "#: <optional_description>"  required for Sphinx autodocumentation
 
-#: Gravitational constant
-#:
-#: DEPRECATED: ``1 * u.gravity`` should be used instead
-GRAVITY = 9.80665 * u.m / u.s ** 2
-
-#: Density of water
-#:
-#: DEPRECATED: ``core.physchem.density_water()`` should be used instead
-WATER_DENSITY = 1000 * u.kg / u.m ** 3
-
-#: Kinematic viscosity of water
-#:
-#: DEPRECATED: ``core.physchem.viscosity_kinematic()`` should be used instead
-WATER_NU = 1 * 10 ** -6 * u.m ** 2 / u.s
-
-#: Atmospheric pressure
-#:
-#: DEPRECATED: ``1 * u.atm`` should be used instead
-ATM_P = 1 * u.atm
-
-#: Average kinematic viscosity of air
-#:
-#: DEPRECATED: viscosity of air should be determined by temperature and
-#: atmospheric pressure
-AIR_NU = 12 * u.mm ** 2 / u.s
-
 #: The ratio of maximum energy dissipation rate in a round jet to the kinetic
 #: energy per time required for the jet to travel a distance equal to its
 #: diameter. See `round jet
