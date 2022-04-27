84e7799c294982e62b85b5b8511febcad9d8f217
henry <henrychen262799@gmail.com>
2022-04-27 16:44:55 -0400

    Fixed not labeling an exception styling issue

--- aguaclara/core/onshape_parser.py
+++ aguaclara/core/onshape_parser.py
@@ -49,7 +49,7 @@ def parse_quantity(q, for_docs=True):
         units_s = units_s * ureg(unit[key_str].lower()) ** unit[val_str]
         try:
             log = math.floor(math.log10(units_s.magnitude))
-        except:
+        except Exception:
             log = 0
         if for_docs:
             if unit[key_str] == "METER" and unit[val_str] == 1:
